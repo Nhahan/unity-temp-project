ac7f2611cd997f3fccb7b5a3c9ab64b03a110cb8
sun <sun@codebrick.co>
2022-06-08 22:58:13 +0900

    init

---
+++ Assets/FollowCamera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    // this camera should be the same as the car's position
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

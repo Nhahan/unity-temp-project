da778ce245e4e1bacaa4d326b3bee95c793e053f
sun <sun@codebrick.co>
2022-06-11 00:01:49 +0900

    Define rigidBody2D

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody2D rigidBody2D;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.LeftArrow)) {
+            rigidBody2D.AddTorque(1);
+        }
     }
 }

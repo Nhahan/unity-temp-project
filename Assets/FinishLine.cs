a70edb1da4caa1d00d5aaaad93d41b76a0dbd2ca
sun <sun@codebrick.co>
2022-06-11 01:44:11 +0900

    Init

---
+++ Assets/FinishLine.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    void private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("You Finished!")
+    }
+}

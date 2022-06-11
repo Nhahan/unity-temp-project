9cf31a13ce72943f98fed0f5814124fbeb28d45a
sun <sun@codebrick.co>
2022-06-12 04:23:11 +0900

    Define GetQuestion()

--- Assets/QuestionScript.cs
+++ Assets/QuestionScript.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuestionScript : MonoBehaviour
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "new Question")]
+public class QuestionScript : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [TextArea(2,6)]
+    [SerializeField] string question = "Enter new question text here";
 
-    // Update is called once per frame
-    void Update()
+    public string GetQuestion()
     {
-        
+        return question;
     }
 }
